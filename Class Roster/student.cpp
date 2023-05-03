a0abc83ba299df2b2fe89b548955f23c2cdf7e23
Xavier Loera Flores <x.loera.tech@gmail.com>
2023-05-03 03:46:34 -0700

    FEAT: Added getter functions for all of the Student class variables;

--- Class Roster/student.cpp
+++ Class Roster/student.cpp
@@ -17,7 +17,37 @@ class Student {
         string lastName;
         string email;
         int age;
-        int daysToCompleteCourse[3];
+        int daysToCompleteCourses[3];
         DegreeProgram degreeProgram;
+    public:
+    string getStudentID(){
+        return studentID;
+    }
+    string getFirstName(){
+        return firstName;
+    }
+    string getLastName(){
+        return lastName;
+    }
+    string getEmail(){
+        return email;
+    }
+    int getAge(){
+        return age;
+    }
+    // Returns a pointer to the daysToCompleteCourses array
+    int * getDaysToCompleteCoursesPointer(){
+        return daysToCompleteCourses;
+    }
+    // Returns an integer to the number of days to complete a course for a specific day
+    int getDaysToCompleteCourse(int index){
+        if( index>2 || index<0 ){
+            return -1;
+        };
+        return daysToCompleteCourses[index];
+    }
+    
+    
+    
     
 };
