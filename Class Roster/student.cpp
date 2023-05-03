6e9f784dea9d28eac4ea8272c69a14543e10c859
Xavier Loera Flores <x.loera.tech@gmail.com>
2023-05-03 01:13:34 -0700

    FEAT: Create student class & header file

---
+++ Class Roster/student.cpp
@@ -0,0 +1,8 @@
+//
+//  student.cpp
+//  Class Roster
+//
+//  Created by Xavier Loera Flores on 5/3/23.
+//
+
+#include "student.h"
---
+++ Class Roster/student.h
@@ -0,0 +1,6 @@
+//
+//  student.h
+//  Class Roster
+//
+//  Created by Xavier Loera Flores on 5/3/23.
+//
