67eccc8093956edd387a7378f5f11f050dff15cb
Xavier Loera Flores <x.loera.tech@gmail.com>
2023-05-03 07:38:29 -0700

    FEAT: Created roster.cpp & roster.h files

---
+++ Class Roster/roster.cpp
@@ -0,0 +1,8 @@
+//
+//  roster.cpp
+//  Class Roster
+//
+//  Created by Xavier Loera Flores on 5/3/23.
+//
+
+#include <stdio.h>
---
+++ Class Roster/roster.h
@@ -0,0 +1,7 @@
+//
+//  roster.h
+//  Class Roster
+//
+//  Created by Xavier Loera Flores on 5/3/23.
+//
+
