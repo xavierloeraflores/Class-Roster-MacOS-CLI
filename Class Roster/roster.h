a3c29ba3aaa91e873606ebff6c200e56b7432d56
Xavier Loera Flores <x.loera.tech@gmail.com>
2023-05-03 08:25:26 -0700

    FIX: Ensured classRosterArray utilizes pointers & minor fix to Roster() function

--- Class Roster/roster.h
+++ Class Roster/roster.h
@@ -14,14 +14,14 @@ using namespace std;
 
 class Roster {
     private:
-    Student classRosterArray[5];
+        Student *classRosterArray[5];
     public:
-        Roster ()
-    void add(string studentID, string firstName, string lastName, string, emailAddress, int age, int daysInCourse1, int daysInCourse2, int daysInCourse3, DegreeProgram degreeProgram);
-    void remove(string studentID);
-    void printAll();
-    void printAverageDaysInCourse(string studentID);
-    void printInvalidEmails();
-    void printByDegreeProgram(DegreeProgram degreeProgram);
+        Roster ();
+        void add(string studentID, string firstName, string lastName, string, emailAddress, int age, int daysInCourse1, int daysInCourse2, int daysInCourse3, DegreeProgram degreeProgram);
+        void remove(string studentID);
+        void printAll();
+        void printAverageDaysInCourse(string studentID);
+        void printInvalidEmails();
+        void printByDegreeProgram(DegreeProgram degreeProgram);
 };
 #endif
