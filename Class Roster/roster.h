22654b507de05e55d12436605f057fb63322b3b9
Xavier Loera Flores <x.loera.tech@gmail.com>
2023-05-03 07:51:31 -0700

    FEAT: Defined Roster class and methods in header file

--- Class Roster/roster.h
+++ Class Roster/roster.h
@@ -5,3 +5,23 @@
 //  Created by Xavier Loera Flores on 5/3/23.
 //
 
+#ifndef ROSTER_H
+#define ROSTER_H
+#include "degree.h"
+#include "student.h"
+
+using namespace std;
+
+class Roster {
+    private:
+    Student classRosterArray[5];
+    public:
+        Roster ()
+    void add(string studentID, string firstName, string lastName, string, emailAddress, int age, int daysInCourse1, int daysInCourse2, int daysInCourse3, DegreeProgram degreeProgram);
+    void remove(string studentID);
+    void printAll();
+    void printAverageDaysInCourse(string studentID);
+    void printInvalidEmails();
+    void printByDegreeProgram(DegreeProgram degreeProgram);
+};
+#endif
