d4508fbe58f251d67fac680b04d7f191a240cda7
Xavier Loera Flores <x.loera.tech@gmail.com>
2023-05-03 08:35:41 -0700

    FEAT: Added (empty) functions to roster.cpp

--- Class Roster/roster.cpp
+++ Class Roster/roster.cpp
@@ -6,3 +6,37 @@
 //
 
 #include <stdio.h>
+#include <iostream>
+#include <string>
+#include "roster.h"
+
+using namespace std;
+
+
+Roster::Roster (){
+    
+}
+
+void Roster::add(string studentID, string firstName, string lastName, string emailAddress, int age, int daysInCourse1, int daysInCourse2, int daysInCourse3, DegreeProgram degreeProgram){
+};
+
+void Roster::remove(string studentID){
+    
+}
+
+void Roster::printAll(){
+    
+}
+
+void Roster::printAverageDaysInCourse(string studentID){
+    
+}
+
+void Roster::printInvalidEmails(){
+    
+}
+
+void Roster::printByDegreeProgram(DegreeProgram degreeProgram){
+    
+}
+
