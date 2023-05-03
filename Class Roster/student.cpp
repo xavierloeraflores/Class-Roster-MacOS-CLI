7f35e5c5f6008631f94ca6e58ea894448a64bcc0
Xavier Loera Flores <x.loera.tech@gmail.com>
2023-05-03 03:26:12 -0700

    FEAT: Created Student class and initialized class variables

--- Class Roster/student.cpp
+++ Class Roster/student.cpp
@@ -4,5 +4,19 @@
 //
 //  Created by Xavier Loera Flores on 5/3/23.
 //
-
+#include <string>
 #include "student.h"
+#include "degree.h"
+
+using namespace std;
+
+class Student {
+private:
+    string studentID;
+    string firstName;
+    string lastName;
+    string email;
+    int age;
+    int daysToCompleteCourse[3];
+    DegreeProgram degreeProgram;
+};
